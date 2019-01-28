57acb169037a1ca2f753d4c6ad150c7669ce429e
Megabiquette <megabiquette@gmail.com>
2019-01-28 23:17:27 +0100

    Added tests

--- app/src/androidTest/java/com/albanfontaine/mynews/InstrumentedTest.java
+++ app/src/androidTest/java/com/albanfontaine/mynews/InstrumentedTest.java
@@ -1,5 +1,7 @@
 package com.albanfontaine.mynews;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.albanfontaine.mynews.Models.ApiResponseTopStories;
@@ -7,14 +9,19 @@ import com.albanfontaine.mynews.Utils.NYTimesStreams;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.junit.Assert.assertEquals;
 
 import io.reactivex.Observable;
 import io.reactivex.observers.TestObserver;
 
+import static org.junit.Assert.*;
 
-public class MainFragmentTest {
-
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class InstrumentedTest {
     @Test
     public void fetchTopStoriesArticlesTest() throws Exception{
         Observable<ApiResponseTopStories> observableArticles =
@@ -30,16 +37,16 @@ public class MainFragmentTest {
         ApiResponseTopStories articlesFetched = testObserver.values().get(0);
 
         // Checks that the responses gets 40 articles
-        assertEquals(articlesFetched.getResults().size(),40);
+        //assertEquals(articlesFetched.getResults().size(),40);
 
         ApiResponseTopStories.Result article = articlesFetched.getResults().get(0);
         // Checks that the article has a title
-        assert (article.getTitle() != null);
+        assertNotNull ("Article has title", article.getTitle());
         // Checks that the article has a date
-        assert (article.getPublishedDate() != null);
+        assertNotNull ("Article has date", article.getPublishedDate());
         // Checks that the article has a section
-        assert (article.getSection() != null);
+        assertNotNull ("Artidle has section", article.getSection());
         // Checks that the article has a url
-        assert (article.getUrl() != null);
+        assertNotNull ("Article has URL", article.getUrl());
     }
 }
