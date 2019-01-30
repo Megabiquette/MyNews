717b569268f12f6fb86b9f7955b50fa77a0100a5
Megabiquette <megabiquette@gmail.com>
2019-01-30 12:57:24 +0100

    Added tests

--- app/src/androidTest/java/com/albanfontaine/mynews/InstrumentedTest.java
+++ app/src/androidTest/java/com/albanfontaine/mynews/InstrumentedTest.java
@@ -15,11 +15,6 @@ import io.reactivex.observers.TestObserver;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class InstrumentedTest {
 
